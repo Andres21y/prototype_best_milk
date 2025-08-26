5ca68cd34bbfc651d8222844cf54e9a64969bd77
Andres21y <andres17vh@gmail.com>
2025-08-25 23:16:26 -0500

    adding login

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,24 @@
-function navigateTo (params) {
-    console.log(params);
+const AppConfig = {
+    name: "MiApp",
+    version: "1.0.0",
+    apiUrl: "https://andres21y.github.io/prototype_best_milk/",
+    routes: {
+        login: "views/login/index.html",
+        signup: "views/signup/index.html",
+        error: "error/index.html",
+        dashboard: "views/dashboard/index.html"
+    }
+};
+
+
+
+function navigateTo(props) {
+    window.location.href = `views/${props}/index.html`;
+    console.log(props);
     
 }
+
+/* Hacer funciones disponibles globalmente*/
+window.navigateTo = Utils.navigateTo;
+window.showToast = Utils.showToast;
+window.validateEmail = Utils.validateEmail;
