e2da8848b69cb1ce70b9fd53ae77455c55eb8823
Andres21y <andres17vh@gmail.com>
2025-09-09 00:29:10 -0500

    adding styles

--- components/Cta.js
+++ components/Cta.js
@@ -4,11 +4,11 @@ const Cta = {
         return `
             <section class="cta-section white" style="height:400px">
                 <div class="carousel">
-    <a class="carousel-item cimg" href="#one!"><img src="../../assets/images/breed/jersey.jpg"></a>
-    <a class="carousel-item cimg" href="#two!"><img src="../../assets/images/breed/ayrshire.jpg"></a>
-    <a class="carousel-item cimg" href="#three!"><img src="../../assets/images/breed/holstein.jpg"></a>
-    <a class="carousel-item cimg" href="#four!"><img src="../../assets/images/cows.jpg"></a>
-    <a class="carousel-item cimg" href="#five!"><img src="../../assets/images/main_ganado.jpeg"></a>
+    <a class="carousel-item cimg" href="#one!"><img src="../assets/images/breed/jersey.jpg"></a>
+    <a class="carousel-item cimg" href="#two!"><img src="../assets/images/breed/ayrshire.jpg"></a>
+    <a class="carousel-item cimg" href="#three!"><img src="../assets/images/breed/holstein.jpg"></a>
+    <a class="carousel-item cimg" href="#four!"><img src="../assets/images/cows.jpg"></a>
+    <a class="carousel-item cimg" href="#five!"><img src="../assets/images/main_ganado.jpeg"></a>
   </div>
             </section>
         `;
